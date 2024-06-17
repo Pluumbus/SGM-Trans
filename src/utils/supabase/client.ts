f51b98f9b6f82edf3b8127716b590acee907b0df
Illarion <74643342+PinkPiton@users.noreply.github.com>
2024-06-17 17:19:59 +0500

    init

---
+++ src/utils/supabase/client.ts
@@ -0,0 +1,38 @@
+import { createClient } from "@supabase/supabase-js";
+
+declare global {
+  interface Window {
+    Clerk: {
+      session?: {
+        getToken: (options: { template: string }) => Promise<string>;
+      };
+    };
+  }
+}
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+function createClerkSupabaseClient() {
+  return createClient(supabaseUrl, supabaseAnonKey, {
+    global: {
+      fetch: async (url, options = {}) => {
+        const clerkToken = await window.Clerk.session?.getToken({
+          template: "SGM-TRANS",
+        });
+
+        const headers = new Headers(options?.headers);
+        headers.set("Authorization", `Bearer ${clerkToken}`);
+
+        return fetch(url, {
+          ...options,
+          headers,
+        });
+      },
+    },
+  });
+}
+
+const supabase = createClerkSupabaseClient();
+
+export default supabase;
