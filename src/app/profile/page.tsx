e55f3283ef5e99ec3d8245158673b2c067c3c124
maduroh <ilya_rupenko@mail.ru>
2024-06-25 17:16:02 +0500

    FIX

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,13 +2,13 @@
 import { SignOutButton } from "@clerk/nextjs";
 import Dashboard from "./dashboard";
 import { Button } from "@nextui-org/react";
-import { AssignRole } from "./assignRole";
+// import { AssignRole } from "./assignRole";
 
 export default function ProfilePage() {
   return (
     <div>
       <Dashboard />
-      <AssignRole />
+      {/* <AssignRole /> */}
       <Button color="danger">
         <SignOutButton>Выйти</SignOutButton>
       </Button>
