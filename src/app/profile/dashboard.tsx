f6911a4c2b85a93c322413a0567e08e7cd16b47b
maduroh <ilya_rupenko@mail.ru>
2024-06-26 10:40:26 +0500

    deploy

--- src/app/profile/dashboard.tsx
+++ src/app/profile/dashboard.tsx
@@ -5,19 +5,16 @@ function Dashboard() {
   const { user } = useUser();
   const role = user?.publicMetadata?.role as string | undefined;
 
-  // Не надо так делать, лучше делать
-  /* 
-    const roleContentMap = {
-      Admin: <div>Admin</div>,
-      user: <div>User</div>,
-      Guest: <div>Guest</div>,
-    };
-
-    и это если без билдера если с ним потом напишу меня клерк азебал
-
-    return roleContentMap[role] || roleContentMap.Guest;
-
-  */
+  // const rolesList = {
+  //   Admin: <div>Ваша роль Админ</div>,
+  //   Logist: <div>Ваша роль Логист</div>,
+  //   LogisctMSC: <div>Ваша роль Логист Москва</div>,
+  //   SuperLogist: <div>Ваша роль Супер Логист</div>,
+  //   WarehouseManager: <div>Ваша роль Зав.Склада</div>,
+  //   WarehouseManagerMSC: <div>Ваша роль Зав.Склада Москва</div>,
+  //   Cashier: <div>Ваша роль Кассир</div>,
+  //   Guest: <div>Ваша роль Гость</div>
+  // };
 
   if (role === "Admin") {
     return <div>Admin</div>;
