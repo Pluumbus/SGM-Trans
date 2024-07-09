3de65facf077e8ac4dfba5533c6a568683f49024
maduroh <ilya_rupenko@mail.ru>
2024-07-09 16:13:27 +0500

    deploy fix, dynamic route in table updated

--- src/app/workflow/[slug]/page.tsx
+++ src/app/workflow/[slug]/page.tsx
@@ -1,23 +1,13 @@
 import { NextPage } from "next";
-import { WeekCard } from "../_feature/WeekCard";
-import { useParams } from "next/navigation";
+import SlugRender from "./baseRender";
 
 interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
-  const { slug } = useParams();
-
-  return (
-    <div>
-      <span className="flex justify-center font-semibold">
-        {slug === "kz" ? "Таблица КЗ" : "Таблица МСК"}
-        {/* TODO Далее уже корректить какой рендер нужен для мск таблички пока что темплейт */}
-      </span>
-      <WeekCard />
-    </div>
-  );
+  return <SlugRender />;
 };
 export default Page;
+
 export async function generateStaticParams() {
   return [];
 }
--- src/app/workflow/trip/[id]/page.tsx
+++ src/app/workflow/trip/[id]/page.tsx
@@ -1,40 +1,8 @@
-"use client";
-import { UTable } from "@/tool-kit/ui";
 import { NextPage } from "next";
-import { useParams } from "next/navigation";
-import { useMemo } from "react";
-import { getBaseColumnsConfig } from "./Table/CargoTable.config";
-import { Cargo } from "../../_feature/types";
-import { UseTableConfig } from "@/tool-kit/ui/UTable/types";
-import mockData from "./Table/mock.data";
+import RenderUiTable from "./tbRender";
 
 const Page: NextPage = ({}) => {
-  const columns = useMemo(() => getBaseColumnsConfig(), []);
-  const config: UseTableConfig<Cargo> = {
-    row: {
-      setRowData(info) {},
-      className: "cursor-pointer",
-    },
-  };
-
-  const mMockData = useMemo(() => {
-    return mockData;
-  }, []);
-  const { id } = useParams();
-
-  return (
-    <div>
-      <div>
-        <span>Номер рейса: {id}</span>
-      </div>
-      <UTable
-        data={mMockData}
-        columns={columns}
-        name="Cargo Table"
-        config={config}
-      />
-    </div>
-  );
+  return <RenderUiTable />;
 };
 
 export default Page;
