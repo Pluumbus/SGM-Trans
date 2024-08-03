fdfb2649b6ae14a48f713601ca46b51090d046cf
maduroh <ilya_rupenko@mail.ru>
2024-08-03 20:21:05 +0500

    config test

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const outputDir = process.env.BRANCH === 'dev' ? 'dev' : '.next';
 
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   distDir: outputDir,
   compiler: {
     reactRemoveProperties: isProduction,
