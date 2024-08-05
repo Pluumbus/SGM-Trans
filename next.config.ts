715701ead45e2fb04b063cbd4ee105ce371adfd9
maduroh <ilya_rupenko@mail.ru>
2024-08-05 14:40:04 +0500

    config chanhes prefer merge

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,23 @@
 import path from 'path';
 import type { NextConfig } from 'next';
-
+const isProduction = process.env.NODE_ENV === "production";
+const outputDir = process.env.BRANCH === "dev" ? "dev" : ".next";
 const nextConfig: NextConfig = {
+  output:"standalone",
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
   typescript: {
     ignoreBuildErrors: true,
+  },
+  distDir: outputDir,
+  compiler: {
+    reactRemoveProperties: isProduction,
+    removeConsole: isProduction,
+    styledComponents: {
+      displayName: !isProduction,
+      minify: isProduction,
+      pure: true,
+    },
   },
   webpack: (config) => {
     config.module.rules.push({
