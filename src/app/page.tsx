bb85a1ef221dad9eff6cf4cd0b42a733c75b9bc4
maduroh <ilya_rupenko@mail.ru>
2024-08-12 18:28:09 +0500

    env test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-
 "use client";
 import { NextPage } from "next";
-
 
 interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
   return (
     <div className=" flex flex-col gap-2">
-      <span>Домашнаяя страница</span>
+      <span>Домашняя страница</span>
+      <span>{process.env.NODE_ENV}</span>
     </div>
   );
 };
--- src/utils/clerk/clerk.ts
+++ src/utils/clerk/clerk.ts
@@ -2,18 +2,17 @@
 "use server";
 import { createClerkClient } from "@clerk/nextjs/server";
 
-// const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
+const clerkPublishableKey = isProduction 
+  ? process.env.CLERK_PUBLISHABLE_KEY 
+  : 'pk_test_cmVmaW5lZC1naWJib24tNDYuY2xlcmsuYWNjb3VudHMuZGV2JA';
 
-// const clerkPublishableKey = isProduction 
-//   ? process.env.CLERK_PUBLISHABLE_KEY 
-//   : 'pk_test_cmVmaW5lZC1naWJib24tNDYuY2xlcmsuYWNjb3VudHMuZGV2JA';
+const clerkSecretKey = isProduction 
+  ? process.env.CLERK_SECRET_KEY 
+  : 'sk_test_Msbz2NEUEPtLVRgJ0yo0H4stPypUg9mzLFRYeKV2to';
 
-// const clerkSecretKey = isProduction 
-//   ? process.env.CLERK_SECRET_KEY 
-//   : 'sk_test_Msbz2NEUEPtLVRgJ0yo0H4stPypUg9mzLFRYeKV2to';
-
-const clerkPublishableKey = process.env.CLERK_PUBLISHABLE_KEY ;
-const clerkSecretKey = process.env.CLERK_SECRET_KEY ;
+// const clerkPublishableKey = process.env.CLERK_PUBLISHABLE_KEY ;
+// const clerkSecretKey = process.env.CLERK_SECRET_KEY ;
 
 const getClerkClient = async () => {
   return createClerkClient({
