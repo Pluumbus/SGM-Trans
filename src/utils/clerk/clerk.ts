83e779ac76ec7bae6f8de223efac275a240cc1aa
maduroh <ilya_rupenko@mail.ru>
2024-08-12 15:10:21 +0500

    test env for prod

--- src/utils/clerk/clerk.ts
+++ src/utils/clerk/clerk.ts
@@ -1,10 +1,9 @@
 /* eslint-disable prettier/prettier */
 "use server";
 import { createClerkClient } from "@clerk/nextjs/server";
-import { getAuth } from "@clerk/nextjs/server";
 
-const clerkSecretKey = process.env.CLERK_SECRET_KEY!;
-const clerkPublishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+const clerkPublishableKey = process.env.CLERK_PUBLISHABLE_KEY;
+const clerkSecretKey = process.env.CLERK_SECRET_KEY;
 
 const getClerkClient = async () => {
   return createClerkClient({
