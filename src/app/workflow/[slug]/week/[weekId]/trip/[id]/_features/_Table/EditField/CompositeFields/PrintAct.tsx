be27150c1cb71fda578c1cf3a08f7403a54ab788
maduroh <ilya_rupenko@mail.ru>
2024-10-03 01:08:09 +0500

    fix

--- src/app/workflow/[slug]/week/[weekId]/trip/[id]/_features/_Table/EditField/CompositeFields/PrintAct.tsx
+++ src/app/workflow/[slug]/week/[weekId]/trip/[id]/_features/_Table/EditField/CompositeFields/PrintAct.tsx
@@ -28,10 +28,11 @@ export const PrintAct = ({ info }: { info: Cell<CargoType, ReactNode> }) => {
   const [values, setValues] = useCompositeStates<Type>(info);
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [isChecked, setIsChecked] = useState(values?.value || false);
-  const actVal = info.row.original.is_act_ready?.value;
+  const actVal = info.row.original.is_act_ready?.value || false;
   const { data, isFetched } = useQuery({
     queryKey: ["getUserByIdForAct"],
-    queryFn: async () => await getUserById(values.user_id),
+    queryFn: async () => await getUserById(values?.user_id),
+    enabled: !!values.user_id,
   });
   const actSign = isFetched && actVal ? data.firstName : "Одобрить";
   const actData: ActType = {
@@ -61,13 +62,13 @@ export const PrintAct = ({ info }: { info: Cell<CargoType, ReactNode> }) => {
         ) : (
           <Checkbox
             isSelected={isChecked}
-            onValueChange={(e) => {
+            onValueChange={() => {
               setIsChecked(true);
               onOpen();
             }}
           >
             {actSign}
-            <ActModalForLigst
+            <GlobalActModal
               isOpen={isOpen}
               onOpenChange={onOpenChange}
               cargoPrice={Number(info.row.original.amount.value)}
@@ -87,7 +88,7 @@ export const PrintAct = ({ info }: { info: Cell<CargoType, ReactNode> }) => {
   );
 };
 
-const ActModalForLigst = ({
+const GlobalActModal = ({
   isOpen,
   onOpenChange,
   cargoPrice,
@@ -124,7 +125,7 @@ const ActModalForLigst = ({
   });
   return (
     <div>
-      {!isChecked && !checkRole(["Кассир", "Админ"]) && (
+      {isChecked && (
         <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
           <ModalContent>
             {(onClose) => (
@@ -135,7 +136,7 @@ const ActModalForLigst = ({
                 <ModalBody>
                   <p>
                     При соглашении у вас спишется стоимость груза с баланса в
-                    размере <b>{`${cargoPrice}`}</b>и появится возможность
+                    размере <b>{`${cargoPrice}`}</b> и появится возможность
                     распечатать Акт приема-выдачи
                   </p>
                 </ModalBody>
