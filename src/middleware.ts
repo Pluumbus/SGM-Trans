1df7acd9a89728f06ddcd6d617ffb8566d47d23c
Ilya <73891063+Pluumbus@users.noreply.github.com>
2024-10-22 21:08:44 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,5 +7,9 @@ const isProtectedRoute = createRouteMatcher(["/workflow(.*)","/(.*)"]);
 // });
 export default clerkMiddleware()
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  // matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 };
