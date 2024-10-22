400f3171d0b69926eae7c9c4a200ce8cdad600a1
Ilya <73891063+Pluumbus@users.noreply.github.com>
2024-10-22 21:08:01 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/workflow(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/workflow(.*)","/(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
-
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth().protect();
+// });
+export default clerkMiddleware()
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
