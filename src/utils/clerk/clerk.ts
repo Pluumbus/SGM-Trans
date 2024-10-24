d94fdff1891e8cb3ae7d95838e34126b0fbf9214
Ilya <73891063+Pluumbus@users.noreply.github.com>
2024-10-24 13:35:28 +0500

    Update clerk.ts

--- src/utils/clerk/clerk.ts
+++ src/utils/clerk/clerk.ts
@@ -2,15 +2,6 @@
 "use server";
 import { createClerkClient } from "@clerk/nextjs/server";
 
-// const isProduction = process.env.NODE_ENV === 'production';
-// const clerkPublishableKey = isProduction 
-//   ? process.env.CLERK_PUBLISHABLE_KEY 
-//   : 'pk_test_cmVmaW5lZC1naWJib24tNDYuY2xlcmsuYWNjb3VudHMuZGV2JA';
-
-// const clerkSecretKey = isProduction 
-//   ? process.env.CLERK_SECRET_KEY 
-//   : 'sk_test_Msbz2NEUEPtLVRgJ0yo0H4stPypUg9mzLFRYeKV2to';
-
 const clerkPublishableKey = process.env.CLERK_PUBLISHABLE_KEY ;
 const clerkSecretKey = process.env.CLERK_SECRET_KEY ;
 
