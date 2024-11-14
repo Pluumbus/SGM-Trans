0173e827f05c341e09578293a61754cb0a3541ee
Ilya <73891063+Pluumbus@users.noreply.github.com>
2024-11-14 18:18:47 +0500

    fix

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,13 +1,14 @@
 "use server";
 
 import { createClient } from "@supabase/supabase-js";
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 async function getSupabaseServer() {
-  const { userId, getToken } = auth();
+
+  const { userId, getToken } = await auth();
   if (!userId) throw new Error("User not authenticated");
 
   const accessToken = await getToken({ template: "SGM-TRANS" });
