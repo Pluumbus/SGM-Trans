b48f14defb5ca9db934c34ff1f3d1e52b9233d50
Ilya <73891063+Pluumbus@users.noreply.github.com>
2024-11-16 18:49:03 +0500

    fix

--- src/app/(backend-logic)/workflow/[slug]/week/[weekId]/trip/[id]/_features/WorkflowBucket/api/index.ts
+++ src/app/(backend-logic)/workflow/[slug]/week/[weekId]/trip/[id]/_features/WorkflowBucket/api/index.ts
@@ -3,53 +3,61 @@ import { customTransliterateToEngl } from "@/components/CustomTranslit/CustomTra
 import { DocumentToViewType, DocumentType } from "./types";
 import supabase from "@/utils/supabase/client";
 
-
-export const uploadFile = async (file: File, weekId : string) => {
- const fName = customTransliterateToEngl(file.name)
+export const uploadFile = async (file: File, weekId: string) => {
+  const fName = customTransliterateToEngl(file.name);
+  const sup = await supabase;
   const { data, error } = await supabase.storage
     .from("workflow-documents")
     .upload(fName, file);
 
-    const url = supabase.storage.from("workflow-documents").getPublicUrl(file.name);
+  const url = supabase.storage
+    .from("workflow-documents")
+    .getPublicUrl(file.name);
 
+  const { data: sData, error: weeksError } = await supabase
+    .from("weeks")
+    .update({ docs: { docUrl: url, originalName: file.name } })
+    .eq("id", weekId);
 
-    const {data:sData, error:weeksError} = await supabase.from("weeks").update({docs : {docUrl: url, originalName: file.name}}).eq("id",weekId)
-
-console.log(sData, "weekId", weekId)
+  console.log(sData, "weekId", weekId);
   if (error || weeksError) {
     throw new Error(error.message || weeksError.message);
-  } 
-  else {
+  } else {
     return data;
   }
 };
 
-export const getFileUrlList = async (): Promise<DocumentToViewType[] | null> => {
-  const {data, error} = await supabase.storage.from("workflow-documents").list();
-  
-  let docsToView ;
-  if(data) {
-      docsToView = data.map((doc) => {
-      const url = supabase.storage.from("workflow-documents").getPublicUrl(doc.name);
-      const docData : DocumentToViewType = {
-        id:doc.id,
-        title:doc.name,
-        docType:doc.metadata.mimetype,
-        publicUrl:url.data.publicUrl,
+export const getFileUrlList = async (): Promise<
+  DocumentToViewType[] | null
+> => {
+  const { data, error } = await supabase.storage
+    .from("workflow-documents")
+    .list();
+
+  let docsToView;
+  if (data) {
+    docsToView = data.map((doc) => {
+      const url = supabase.storage
+        .from("workflow-documents")
+        .getPublicUrl(doc.name);
+
+      const docData: DocumentToViewType = {
+        id: doc.id,
+        title: doc.name,
+        docType: doc.metadata.mimetype,
+        publicUrl: url.data.publicUrl,
         created_at: doc.created_at,
-      }
-      return docData
-    })
-  } 
-  if(error) {
-    throw new Error(error.message)
-  } else { 
+      };
+      return docData;
+    });
+  }
+  if (error) {
+    throw new Error(error.message);
+  } else {
     return docsToView;
   }
-}
+};
 
-export const deleteFile = async (path : string) => {
-await supabase.storage
-    .from("workflow-documents")
-    .remove([path]);
-}
+export const deleteFile = async (path: string) => {
+  await supabase.storage.from("workflow-documents").remove([path]);
+};
