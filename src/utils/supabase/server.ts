ab4860129533a6cb11c0bca5b7b587819518d1f6
Ilya <73891063+Pluumbus@users.noreply.github.com>
2024-11-16 18:33:33 +0500

    fix

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -7,12 +7,12 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 async function getSupabaseServer() {
-
   const { userId, getToken } = await auth();
   if (!userId) throw new Error("User not authenticated");
 
   const accessToken = await getToken({ template: "SGM-TRANS" });
 
+  console.log(accessToken);
   return createClient(supabaseUrl, supabaseAnonKey, {
     global: { headers: { Authorization: `Bearer ${accessToken}` } },
   });
