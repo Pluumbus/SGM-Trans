e718e074edc0d0b2609575f423f2722fca5a4b04
Illarion <74643342+PinkPiton@users.noreply.github.com>
2024-11-28 15:48:07 +0500

    upd

--- src/tool-kit/ui/UTable/helpers/renderRows.tsx
+++ src/tool-kit/ui/UTable/helpers/renderRows.tsx
@@ -1,14 +1,38 @@
-import { Divider, TableCell, TableRow } from "@nextui-org/react";
+import { TableCell, TableRow } from "@nextui-org/react";
 import { Table, flexRender } from "@tanstack/react-table";
 import { RowConfigProps } from "../types";
+import { cx } from "class-variance-authority";
+import { useState } from "react";
 
 export const renderRows = <T,>(
   tInstance: Table<T>,
   config: RowConfigProps<T>
 ) => {
+  const [highlightedRows, setHighlightedRows] = useState<Set<string>>(
+    new Set()
+  );
+
+  const toggleHighlight = (rowId: string) => {
+    setHighlightedRows((prev) => {
+      const newSet = new Set(prev);
+      if (newSet.has(rowId)) {
+        newSet.delete(rowId);
+      } else {
+        newSet.add(rowId);
+      }
+      return newSet;
+    });
+  };
   return tInstance.getRowModel().rows.map((row, i) => (
     <TableRow
-      className={config.setClassNameOnRow && config?.setClassNameOnRow(row)}
+      className={cx(
+        config.setClassNameOnRow && config?.setClassNameOnRow(row),
+        highlightedRows.has(row.id) && "bg-orange-300"
+      )}
+      onContextMenu={(e) => {
+        e.preventDefault();
+        toggleHighlight(row.id);
+      }}
       key={row.id}
     >
       {row.getVisibleCells().map((cell) => (
