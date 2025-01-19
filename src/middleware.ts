2f70be30fcec8cd69b464027da85e234aec0025e
Ilya <73891063+Pluumbus@users.noreply.github.com>
2025-01-19 18:35:34 +0500

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,6 @@ export default clerkMiddleware(async (auth, req) => {
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/workflow/:path*",
     "/(api|trpc)(.*)",
   ],
 };
