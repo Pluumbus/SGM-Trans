09440804a00c0afdc04adec86a7d540b601236ad
Ilya <73891063+Pluumbus@users.noreply.github.com>
2025-01-19 21:26:35 +0500

    Update server.ts

--- src/app/(backend-logic)/requests/_api/server.ts
+++ src/app/(backend-logic)/requests/_api/server.ts
@@ -4,12 +4,12 @@ import {
   ClientRequestTypeDTO,
 } from "@/app/(client-logic)/client/types";
 import supabase from "@/utils/supabase/client";
-import { currentUser } from "@clerk/nextjs/server";
+import { useUser } from "@clerk/nextjs";
 
 export const getRequests = async (): Promise<
   Array<ClientRequestTypeDTO & { trip_id: number }>
 > => {
-  const user = await currentUser();
+  const { user } = useUser();
   const { data, error } = await supabase
     .from("requests")
     .select("*")
@@ -28,11 +28,11 @@ export const setRequestStatus = async ({
   reqId: number;
   status: ClientRequestStatus;
 }): Promise<ClientRequestTypeDTO[]> => {
-  const { id } = await currentUser();
+  const { user } = useUser();
   const { data, error } = await supabase
     .from("requests")
     .update({
-      logist_id: id,
+      logist_id: user?.id!,
       status: status,
     })
     .eq("id", reqId)
