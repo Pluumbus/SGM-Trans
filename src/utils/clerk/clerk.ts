91ba831dfd03556d5011c071b4d3de73c52d49d3
Ilya <73891063+Pluumbus@users.noreply.github.com>
2025-01-19 22:52:52 +0500

    Update clerk.ts

--- src/utils/clerk/clerk.ts
+++ src/utils/clerk/clerk.ts
@@ -2,8 +2,10 @@
 
 import { createClerkClient } from "@clerk/nextjs/server";
 
-const clerkPublishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-const clerkSecretKey = process.env.CLERK_SECRET_KEY!;
+// const clerkPublishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+// const clerkSecretKey = process.env.CLERK_SECRET_KEY!;
+const clerkPublishableKey = "pk_live_Y2xlcmsuc2dtLXRyYW5zLmNvbSQ";
+const clerkSecretKey = "sk_live_NzMWVVgm67HJP3vuMYNNpzKF2w5y2IYzDwq9dFDlXF";
 
 const getClerkClient = async () => {
   return createClerkClient({
