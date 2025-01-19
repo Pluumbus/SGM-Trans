ab76d552ede95a2820f586d635472d2537692cdb
Ilya <73891063+Pluumbus@users.noreply.github.com>
2025-01-19 22:18:00 +0500

    fix getUsersList

--- src/lib/references/clerkUserType/getUserList.ts
+++ src/lib/references/clerkUserType/getUserList.ts
@@ -1,9 +1,10 @@
 "use server";
 import { UsersList } from "@/lib/references/clerkUserType/types";
+import getClerkClient from "@/utils/clerk/clerk";
 import { clerkClient, User } from "@clerk/nextjs/server";
 
 export const getUserList = async () => {
-  const users = await (await clerkClient()).users.getUserList({ limit: 99 });
+  const users = await (await getClerkClient()).users.getUserList({ limit: 99 });
   const userList = users.data.map((user: User) => ({
     id: user.id,
     userName: user.fullName || "Имя отсутствует",
