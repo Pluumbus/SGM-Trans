e1c8b0dd931b0d724be7bc8890d6132450a4db37
Illarion <74643342+PinkPiton@users.noreply.github.com>
2025-01-20 09:10:30 +0500

    1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/workflow(.*)"]);
@@ -9,6 +11,5 @@ export default clerkMiddleware(async (auth, req) => {
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
   ],
 };
