341765687046da1727359a027bbd9faa05c05547
Illarion <74643342+PinkPiton@users.noreply.github.com>
2025-01-20 09:09:51 +0500

    1

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,28 +1,24 @@
 "use server";
 
 import { createClient } from "@supabase/supabase-js";
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { getSchema } from "./getSchema";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 async function getSupabaseServer() {
-  const { getToken } = await auth();
-  // if (!user?.id) redirectToSignIn();
+  const { userId, getToken } = await auth();
+  if (!userId) throw new Error("User not authenticated");
 
-  try {
-    const accessToken = await getToken({ template: "SGM-TRANS" });
+  const accessToken = await getToken({ template: "SGM-TRANS" });
 
-    return createClient(supabaseUrl, supabaseAnonKey, {
-      db: {
-        schema: getSchema(),
-      },
-      global: { headers: { Authorization: `Bearer ${accessToken}` } },
-    });
-  } catch (error) {
-    throw new Error(error);
-  }
+  return createClient(supabaseUrl, supabaseAnonKey, {
+    db: {
+      schema: getSchema(),
+    },
+    global: { headers: { Authorization: `Bearer ${accessToken}` } },
+  });
 }
 
 export default getSupabaseServer;
