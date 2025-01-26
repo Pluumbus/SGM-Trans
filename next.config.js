47a9922a957faf4746503d59f0e1e88c43d8cdd9
Ilya <73891063+Pluumbus@users.noreply.github.com>
2025-01-26 19:57:41 +0500

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const isProduction = process.env.NODE_ENV === "production";
-const outputDir = process.env.BRANCH === "dev" ? "dev" : ".next";
+// const outputDir = process.env.BRANCH === "dev" ? "dev" : ".next";
+const outputDir = isProduction ? ".next" : "dev";
+
 
 const nextConfig = {
   output: "standalone",
