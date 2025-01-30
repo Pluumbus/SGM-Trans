61bfb16bb79eed269f5281c1100ed7ecb5c844f8
Ilya <73891063+Pluumbus@users.noreply.github.com>
2025-01-30 16:59:15 +0500

    Update page.tsx

--- src/app/(backend-logic)/sgm-mechanic/page.tsx
+++ src/app/(backend-logic)/sgm-mechanic/page.tsx
@@ -191,10 +191,22 @@ const ItemTitle = ({ e }: { e }) => {
                 <DetailIcon name={el.name} />
               </span>
               <span>
-                {getSeparatedNumber(
+                {/* {getSeparatedNumber(
                   Number(el.mileage.next_mileage) !== 0
                     ? Number(el.mileage.next_mileage) -
                         Number(e.omnicommData && e.omnicommData[0].ccan?.spn245)
+                    : 0
+                )} */}
+                {/* {getSeparatedNumber(
+                  Number(el.mileage.next_mileage) !== 0
+                    ? Number(e.omnicommData && e.omnicommData[0].ccan?.spn245) -
+                        Number(el.mileage.last_mileage)
+                    : 0
+                )} */}
+                {getSeparatedNumber(
+                  Number(el.mileage.next_mileage) !== 0
+                    ? Number(el.mileage.next_mileage) -
+                        Number(el.mileage.last_mileage)
                     : 0
                 )}
               </span>
